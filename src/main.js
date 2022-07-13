d206c5de71bdb3fc5fc3a93a651a849d4fa1d9a0
Nehoray Genis <bvurth000100@gmail.com>
2022-07-13 15:25:41 +0300

    changed path

--- src/main.js
+++ src/main.js
@@ -1,14 +1,14 @@
 import { createApp } from "vue"
 import App from "./App.vue"
-import "./styles/styles.css"
-import "./styles/styles.scss"
+import "@/styles/styles.css"
+import "@/styles/styles.scss"
 import { focusDirective, rainbowDirective, customOnDirective, clickOutsideDirective } from "./directives"
 import ElementPlus from 'element-plus'
 import 'element-plus/dist/index.css'
 import VueGoogleMaps from '@fawmi/vue-google-maps'
 
-import router from "./router"
-import { myStore } from "./store/store.js"
+import router from "@/router"
+import { myStore } from "@/store/store.js"
 
 const app = createApp(App)
 app.config.globalProperties.$filters = {
